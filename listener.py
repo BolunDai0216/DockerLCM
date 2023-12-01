9d1feabb2e87cfbe5e9cff2aa0e0cabae5026beb
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2023-12-01 08:20:26 -0500

    initial commit

---
+++ listener.py
@@ -0,0 +1,41 @@
+import select
+import time
+
+import lcm
+
+from exlcm import example_t
+
+
+def my_handler(channel, data):
+    msg = example_t.decode(data)
+    print('Received message on channel "%s"' % channel)
+    print("   timestamp   = %s" % str(msg.timestamp))
+    print("   position    = %s" % str(msg.position))
+    print("   orientation = %s" % str(msg.orientation))
+    print("   ranges: %s" % str(msg.ranges))
+    print("   name        = '%s'" % msg.name)
+    print("   enabled     = %s" % str(msg.enabled))
+    print("")
+
+
+def main():
+    lc = lcm.LCM()
+    subscription = lc.subscribe("EXAMPLE", my_handler)
+
+    try:
+        while True:
+            file_descriptor = lc.fileno()
+
+            # Waiting for reading readiness (data available)
+            ready_to_read, _, _ = select.select([file_descriptor], [], [], 0)
+
+            if len(ready_to_read) != 0:
+                lc.handle()
+            else:
+                print(f"[{time.time()}]: \t No data available")
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
